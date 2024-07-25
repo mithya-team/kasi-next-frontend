c58837a5a839d9843fcf63c655a5f7bc02c7e4e5
sanjaykumaryada <2408sanjaykumar@gmail.com>
2024-07-25 12:21:48 +0530

    route constant created

---
+++ src/constant/route.ts
@@ -0,0 +1,17 @@
+export const ROUTE = {
+  USER_ROUTE: {
+    path: '/user/[userId]',
+  },
+  USER_LIST_ROUTE: {
+    path: '/users',
+  },
+  SCHEDULE_ROUTE: {
+    path: '/schedule',
+  },
+  SETTING_ROUTE: {
+    path: '/settings',
+  },
+  SUBSCRIPTION_ROUTE: {
+    path: '/subscription',
+  },
+};
