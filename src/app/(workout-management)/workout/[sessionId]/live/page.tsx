d83f077bf7418ad64ccd5d1af90b20970ad2409e
Piyush Arya <piyush@mithyalabs.com>
2024-07-27 18:53:39 +0530

    add browser calling feature

--- src/app/(workout-management)/workout/[sessionId]/live/page.tsx
+++ src/app/(workout-management)/workout/[sessionId]/live/page.tsx
@@ -1,8 +1,15 @@
 'use client';
 import { FC } from 'react';
 
+import Call from '@/features/BrowserCall';
+
 const Page: FC = () => {
-  return <div className='text-white'>live screen</div>;
+  return (
+    <div className='text-white'>
+      live screen
+      <Call phoneNumber='+918777250782' />
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/features/BrowserCall/index.tsx
@@ -0,0 +1,272 @@
+import { Call, Device } from '@twilio/voice-sdk';
+import React, { useEffect, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { request } from '@/lib/axios/request';
+import logger from '@/lib/logger';
+import { formatTime } from '@/lib/utils';
+
+import Button from '@/components/Buttons';
+import Typo from '@/components/typography/Typo';
+
+export interface ICallBlockProps {
+  phoneNumber: string;
+}
+
+const CallComponent: React.FC<ICallBlockProps> = ({ phoneNumber }) => {
+  const [, setToken] = useState<string>('');
+  const [device, setDevice] = useState<Device | null>(null);
+  const [isCalling, setIsCalling] = useState<boolean>(false);
+  const [isReady, setIsReady] = useState<boolean>(false);
+  const [speakerDevices, setSpeakerDevices] = useState<MediaDeviceInfo[]>([]);
+  const [ringtoneDevices, setRingtoneDevices] = useState<MediaDeviceInfo[]>([]);
+  const [selectedSpeakerDevice, setSelectedSpeakerDevice] = useState<
+    string | null
+  >(null);
+  const [selectedRingtoneDevice, setSelectedRingtoneDevice] = useState<
+    string | null
+  >(null);
+  const [audioSelectionVisible, setAudioSelectionVisible] =
+    useState<boolean>(false);
+  const [currentCall, setCurrentCall] = useState<Call | null>(null);
+  const [isMuted, setIsMuted] = useState<boolean>(false);
+  const [timer, setTimer] = useState<number>(0);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (device) {
+      addDeviceListeners(device);
+    }
+  }, [device]);
+
+  useEffect(() => {
+    if (isCalling && currentCall) {
+      const hangupButton = document.getElementById('button-hangup-outgoing');
+      if (hangupButton) {
+        hangupButton.onclick = () => {
+          log('Hanging up ...');
+          currentCall.disconnect();
+        };
+      }
+
+      if (!timerRef.current) {
+        timerRef.current = setInterval(() => {
+          setTimer((prev) => prev + 1);
+        }, 1000);
+      }
+    } else {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+        timerRef.current = null;
+      }
+    }
+  }, [isCalling, currentCall]);
+
+  const startupClient = async () => {
+    log('Requesting Access Token...');
+    try {
+      const data = await request<{ token: string; identity: string }>(
+        '/calls/token',
+        { method: 'GET' },
+      );
+      log('Got a token.');
+      setToken(data.token);
+      initializeDevice(data.token);
+    } catch (err) {
+      console.error(err);
+      log('An error occurred. See your browser console for more information.');
+    }
+  };
+
+  const initializeDevice = (token: string) => {
+    log('Initializing device');
+    const newDevice = new Device(token, {
+      logLevel: 2,
+      codecPreferences: [Call.Codec.Opus, Call.Codec.PCMU],
+      closeProtection: true,
+    });
+    setDevice(newDevice);
+    newDevice.register();
+  };
+
+  const addDeviceListeners = (device: Device) => {
+    device.on('registered', async () => {
+      log('Twilio.Device Ready to make and receive calls!');
+      setIsReady(true);
+      await handleGetAudioDevices(); // Automatically fetch audio devices when device is ready
+    });
+
+    device.on('error', (error) => {
+      log('Twilio.Device Error: ' + error.message);
+    });
+
+    device.audio?.on('deviceChange', updateAllAudioDevices);
+
+    if (device.audio?.isOutputSelectionSupported) {
+      setAudioSelectionVisible(true);
+    }
+  };
+
+  const makeOutgoingCall = async () => {
+    const params = { To: phoneNumber };
+
+    if (device) {
+      log(`Attempting to call ${params.To} ...`);
+      const call = await device.connect({ params });
+
+      call.on('accept', () => updateUIAcceptedOutgoingCall());
+      call.on('disconnect', updateUIDisconnectedOutgoingCall);
+      call.on('cancel', updateUIDisconnectedOutgoingCall);
+
+      setCurrentCall(call);
+      setIsCalling(true);
+    } else {
+      log('Unable to make call.');
+    }
+  };
+
+  const updateUIAcceptedOutgoingCall = () => {
+    log('Call in progress ...');
+    setIsCalling(true);
+  };
+
+  const updateUIDisconnectedOutgoingCall = () => {
+    log('Call disconnected.');
+    setIsCalling(false);
+    setCurrentCall(null);
+    setTimer(0);
+  };
+
+  const log = (message: string) => {
+    logger(message, 'StartCall.tsx line 150');
+  };
+
+  const updateAllAudioDevices = () => {
+    if (device) {
+      logger(
+        { message: 'updateAllAudioDevices invoked on device change' },
+        'index.tsx line 359',
+      );
+      const speakerDevices: MediaDeviceInfo[] = Array.from(
+        device.audio?.speakerDevices.get() || [],
+      );
+      const ringtoneDevices: MediaDeviceInfo[] = Array.from(
+        device.audio?.ringtoneDevices.get() || [],
+      );
+      logger({ speakerDevices, ringtoneDevices }, 'StartCall.tsx line 396');
+      setSpeakerDevices(speakerDevices);
+      setRingtoneDevices(ringtoneDevices);
+    }
+  };
+
+  const handleGetAudioDevices = async () => {
+    try {
+      await navigator.mediaDevices.getUserMedia({ audio: true });
+    } catch (err) {
+      toast.error('Allow Microphone access');
+    }
+
+    updateAllAudioDevices();
+  };
+
+  const handleSpeakerDeviceChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    const selectedDeviceId = event.target.value;
+    setSelectedSpeakerDevice(selectedDeviceId);
+    device?.audio?.speakerDevices.set([selectedDeviceId]);
+  };
+
+  const handleRingtoneDeviceChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    const selectedDeviceId = event.target.value;
+    setSelectedRingtoneDevice(selectedDeviceId);
+    device?.audio?.ringtoneDevices.set([selectedDeviceId]);
+  };
+
+  const handleToggleAudioSelection = () => {
+    setAudioSelectionVisible((prev) => !prev);
+  };
+
+  const handleMuteToggle = () => {
+    if (currentCall) {
+      const newMuteState = !isMuted;
+      currentCall.mute(newMuteState);
+      setIsMuted(newMuteState);
+    }
+  };
+
+  return (
+    <div className='flex gap-4 border border-gray-300 p-4 w-fit rounded-lg'>
+      {isReady && !isCalling && (
+        <Button id='button-call' onClick={makeOutgoingCall}>
+          Start Call
+        </Button>
+      )}
+
+      {isCalling && (
+        <>
+          <Button id='button-hangup-outgoing'>Hang Up</Button>
+          <Button onClick={handleMuteToggle}>
+            {isMuted ? 'Unmute' : 'Mute'}
+          </Button>
+          <Typo>{formatTime(timer)}</Typo>
+        </>
+      )}
+
+      {audioSelectionVisible && (
+        <div id='output-selection' className='flex gap-4'>
+          <div>
+            <Typo>Speaker</Typo>
+
+            <select
+              className='inline-block font-primary text-xs rounded-xl font-semibold bg-gray-900 w-[10vw] text-white p-2'
+              id='speaker-devices'
+              value={selectedSpeakerDevice || ''}
+              onChange={handleSpeakerDeviceChange}
+            >
+              {speakerDevices.map((device) => (
+                <option key={device.deviceId} value={device.deviceId}>
+                  {device.label}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <Typo>Ringtone Device</Typo>
+            <select
+              className='inline-block font-primary text-xs rounded-xl font-semibold bg-gray-900 w-[10vw] text-white p-2'
+              id='ringtone-devices'
+              value={selectedRingtoneDevice || ''}
+              onChange={handleRingtoneDeviceChange}
+            >
+              {ringtoneDevices.map((device) => (
+                <option key={device.deviceId} value={device.deviceId}>
+                  {device.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
+
+      {isReady && (
+        <Button onClick={handleToggleAudioSelection}>
+          {audioSelectionVisible
+            ? 'Hide Audio Selection'
+            : 'Show Audio Selection'}
+        </Button>
+      )}
+
+      {!isReady && (
+        <div className='flex gap-5'>
+          <Button onClick={startupClient}>Start Client</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CallComponent;
