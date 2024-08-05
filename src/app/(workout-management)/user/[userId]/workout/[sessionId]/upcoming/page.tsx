4da84cbeeceffb1aa78605ba735c75c7f44cbe4d
sanjaykumaryada <2408sanjaykumar@gmail.com>
2024-08-05 23:49:57 +0530

    live screen href added

--- src/app/(workout-management)/user/[userId]/workout/[sessionId]/upcoming/page.tsx
+++ src/app/(workout-management)/user/[userId]/workout/[sessionId]/upcoming/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 import dayjs from 'dayjs';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { FC, useMemo } from 'react';
 
 import { cn } from '@/lib/utils';
@@ -19,6 +21,9 @@ const timerLabelClass =
 const timerContainerClass = 'flex flex-col gap-[5px] w-1/3 ';
 
 const UpcomingRunScreen: FC = () => {
+  const pathname = usePathname();
+  const clientId = pathname.split('/')[2];
+  const sessionId = pathname.split('/')[4];
   const { workoutSessionDetails } = useStoreState(
     ({ WorkoutStore: { workoutSessionDetails } }) => ({
       workoutSessionDetails,
@@ -73,7 +78,10 @@ const UpcomingRunScreen: FC = () => {
             session begins.
           </Typo>
         ) : (
-          <div className='px-32 py-5 bg-gray-800 rounded-xl'>
+          <Link
+            href={`/user/${clientId}/workout/${sessionId}/live`}
+            className='px-32 py-5 bg-gray-800 rounded-xl'
+          >
             <StartCallButton
               disabled={isPast}
               className={cn('p-3.5', { ['opacity-60']: isPast })}
@@ -82,7 +90,7 @@ const UpcomingRunScreen: FC = () => {
             >
               Start Call
             </StartCallButton>
-          </div>
+          </Link>
         )}
       </div>
     </div>
