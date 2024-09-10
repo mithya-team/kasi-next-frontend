c98c7d9ddb751351a63fdeb89a10a29ffe91a41b
sanjaykumaryada <2408sanjaykumar@gmail.com>
2024-09-10 16:50:40 +0530

    style update for reps view metrics card

--- src/features/MetricLayouts/RepsView.tsx
+++ src/features/MetricLayouts/RepsView.tsx
@@ -17,51 +17,60 @@ const RepsView: React.FC<RepsViewProps> = ({ title, reps }) => {
           {title}
         </div>
       )}
-      {reps?.map((rep) => (
-        <div key={rep.id} className='space-y-2 p-5 bg-gray-800 rounded-lg mb-4'>
-          <div className='flex flex-col items-center justify-between text-white text-sm'>
-            <div className='flex flex-row w-full justify-between items-center'>
-              <div className='flex flex-row gap-2.5'>
-                <Typo>{rep.name}</Typo>
-                {/* {isLive ? (
+      {reps?.map((rep) => {
+        return (
+          <div
+            key={rep.id}
+            className='space-y-2 p-5 bg-gray-800 rounded-lg mb-4'
+          >
+            <div className='flex flex-col items-center justify-between text-white text-sm'>
+              <div className='flex flex-row w-full justify-between items-center'>
+                <div className='flex flex-row gap-2.5'>
+                  <Typo>{rep.name}</Typo>
+                  {/* {isLive ? (
                   <WorkoutStatus status={WorkoutSessionStatus.RUNNING} />
                 ) : (
                   <></>
                 )} */}
-                {/* Removing this since it demands extra logic */}
+                  {/* Removing this since it demands extra logic */}
+                </div>
+                <div className='text-gray-300 text-right'>{rep.value}</div>
               </div>
-              <div className='text-gray-300 text-right'>{rep.value}</div>
+              {rep?.laps?.length ? (
+                <hr className='border-gray-600 w-full my-4' />
+              ) : null}
             </div>
-            <hr className='border-gray-600 w-full my-4' />
-          </div>
-          {rep.laps?.map((lap, idx) => (
-            <div
-              key={lap.id}
-              className='flex flex-col items-center justify-between text-gray-500 text-xs'
-            >
-              <div className='flex flex-row w-full justify-between'>
-                <div className='flex-1'>{lap.name}</div>
-                <div>{lap.value}</div>
-              </div>
+            {rep.laps?.map((lap, idx) => {
+              return (
+                <div
+                  key={lap.id}
+                  className='flex flex-col items-center justify-between text-gray-500 text-xs'
+                >
+                  <div className='flex flex-row w-full justify-between'>
+                    <div className='flex-1'>{lap.name}</div>
+                    <div>{lap.value}</div>
+                  </div>
 
-              {rep?.laps?.length && idx < rep?.laps?.length - 1 ? (
-                <hr className='border-gray-600 my-3 opacity-60 w-full' />
-              ) : (
-                <></>
-              )}
-            </div>
-          ))}
-          {rep.recovery && (
-            <div className='flex items-center'>
-              <div className='flex-1 flex justify-center'>
-                <span className='text-white text-xs text-center'>
+                  {rep?.recovery &&
+                  (rep?.laps?.length ?? 0) > 1 &&
+                  idx < (rep?.laps?.length ?? 0) - 1 ? (
+                    <hr className='border-gray-600 my-3 opacity-60 w-full' />
+                  ) : null}
+                </div>
+              );
+            })}
+            {rep.recovery && (
+              <div className='flex items-center gap-4 justify-center'>
+                <div className='flex-1 h-[1px] bg-gray-500' />
+                <div className='text-gray-500 w-[6.5rem] text-sm text-center whitespace-nowrap'>
                   Recovery: {rep.recovery}
-                </span>
+                </div>
+                <div className='flex-1 h-[1px] bg-gray-500' />
               </div>
-            </div>
-          )}
-        </div>
-      ))}
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
