0a5ee5777d4e4eafccf9f3c2f9efce7e7dc54009
sanjaykumaryada <2408sanjaykumar@gmail.com>
2024-10-21 12:19:27 +0530

    auth modal padding and scrolling issue fixed

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,24 +4,31 @@ import { cn } from '@/lib/utils';
 
 const AuthLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className={cn('fixed inset-0  z-[300] bg-gray-900 ')}>
+    <div className={cn('fixed inset-0 z-[300] bg-gray-900')}>
       <div className='h-[50vh] w-[30vw] absolute rotate-90 -top-[100px] -left-[100px] border-2 border-yellow-400 bg-violet-300 opacity-20 rounded-full' />
       <div className='h-[50vh] w-[30vw] absolute rotate-90 -top-[100px] -right-[100px] border bg-green-300 opacity-20 rounded-full' />
       <div className='h-[50vh] w-[30vw] absolute rotate-90 -bottom-[100px] -left-[100px] border bg-green-300 opacity-20 rounded-full' />
       <div className='h-[50vh] w-[30vw] absolute rotate-90 -bottom-[100px] -right-[100px] border bg-violet-300 opacity-20 rounded-full' />
+
       <div
         className={cn('flex justify-center items-center backdrop-blur-[160px]')}
       >
         <div className='min-w-full min-h-screen' />
         <div
           className={cn(
-            'bg-gray-800 absolute flex flex-col rounded-[20px] py-10 px-[52px] max-w-[100vw] max-h-[100vh] border gap-7 border-gray-500 w-[28.5rem]',
+            'bg-gray-800 absolute flex flex-col rounded-[20px] max-w-[100vw] max-h-[100vh] border gap-7 border-gray-500 w-[30rem]',
           )}
         >
-          {children}
+          <div
+            className='overflow-y-auto py-10 px-[52px]'
+            style={{ maxHeight: 'calc(100vh - 80px)' }} // To ensure padding from top and bottom
+          >
+            {children}
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default AuthLayout;
